8872fa94ac0676cb2c54017abed94db4ff7329d7
prafullakumarsahu <prafullakumarsahu001@gmail.com>
2020-10-11 05:48:57 +0530

    Implement Producer Consumer in thread

---
+++ producerconsumer/App.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package producerconsumer;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author prafullakumarsahu
+ *
+ */
+public class App {
+
+	private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(10);
+	private static Random random = new Random();
+
+	/**
+	 * @param args
+	 * @throws InterruptedException
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					producer();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					consumer();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		});
+
+		t1.start();
+		t2.start();
+
+		t1.join();
+		t2.join();
+	}
+
+	private static void producer() throws InterruptedException {
+		while (true) {
+			queue.put(random.nextInt(100));
+		}
+	}
+
+	private static void consumer() throws InterruptedException {
+		while (true) {
+			Thread.sleep(100);
+			if (random.nextInt(10) == 0) {
+				Integer value = queue.take();
+
+				System.out.println("Taken value: " + value + " || queue size: " + queue.size());
+			}
+		}
+	}
+
+}
